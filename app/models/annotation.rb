f1a5d9e1f3ebb76f22e5d42beb90a39333ead290
MicahW <micah.wallberg@gmail.com>
2017-10-30 17:36:51 -0400

    added annotation methods

--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -6,4 +6,34 @@ class Annotation < ApplicationRecord
       errors.add(:name, :invalid)
     end
   end
+  
+  #adds a new annotation or modifies the old one
+  #by replacing non nil paramenters with thier atributes
+  def self.add_annotation(user,dagr,name,keywords)
+    return false if !user.has_dagr?(dagr)
+    has = UserHas.where(users_id: user.id, dagrs_guid: dagr.guid).take
+    #not existing annotation add this one
+    if has.annotations_id == nil
+      annotation = Annotation.create(name: name, keywords: keywords)
+      UserHas.update(has.id,annotations_id: annotation.id)
+      return annotation
+    else
+      #existing annotation
+      annotation = Annotation.find(has.annotations_id)
+      
+      name ||= annotation.name
+      keywords ||= annotation.keywords
+      
+      return Annotation.update(annotation.id,name: name, keywords: keywords)
+    end
+  end
+  
+  def self.remove_annotation(user,dagr)
+    return false if !user.has_dagr?(dagr)
+    has = UserHas.where(users_id: user.id, dagrs_guid: dagr.guid).take
+    UserHas.update(has.id,annotations_id: nil)
+    Annotation.destroy(has.annotations_id) if has.annotations_id
+  end
+ 
+      
 end
--- test/models/annotation_test.rb
+++ test/models/annotation_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
 class AnnotationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   def setup
+    @bob = users(:bob)
+    @alice = users(:alice)
+    @dagr = dagrs(:cat)
+    @dagr_annotated = dagrs(:annotated)
+    @shared = dagrs(:shared_dagr)
+    @category = categories(:bobs_music)
+    @sue = users(:sue)
+    
+    @q1 = dagrs(:query1)
+    @q2 = dagrs(:query2)
+    @q3 = dagrs(:query3)
+    @q4 = dagrs(:query4)
+    @q5 = dagrs(:query5)
+  end
+  
+  test "add annotations" do
+    assert_difference 'Annotation.count', 0 do
+      Annotation.add_annotation(@bob,@dagr,nil,nil)
+    end
+    
+    assert_difference 'Annotation.count', 1 do
+      Annotation.add_annotation(@bob,@dagr,"name","k1,k2,k3")
+    end
+    
+    assert_difference 'Annotation.count', 0 do
+      ann = Annotation.add_annotation(@bob,@dagr,"new_name",nil)
+      assert_equal("new_name",ann.name)
+      assert_equal("k1,k2,k3",ann.keywords)
+      assert_equal("k1,k2,k3",ann.keywords)
+    end
+    
+    assert_difference 'Annotation.count', -1 do
+      Annotation.remove_annotation(@bob,@dagr)
+    end
+    
+  end
+    
 end
